1c092035ca216ba968f1ef97668089da6af5808c
timothyjamesmarias <timothyjamesmarias@gmail.com>
2026-05-10 16:00:19 -0700

    update: static ip

--- configuration.nix
+++ configuration.nix
@@ -29,6 +29,14 @@
 
   networking.hostName = "server";
 
+  # Static IP
+  networking.interfaces.enp86s0.ipv4.addresses = [{
+    address = "192.168.1.7";
+    prefixLength = 24;
+  }];
+  networking.defaultGateway = "192.168.1.1";
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "25.11";
 }
